dd25e18949dd22f6b3802d0b112d7c27ae0b29ec
piedad bull <pbull@miuandes.cl>
2019-08-19 12:19:31 -0400

    Codigos videos if, else y elif
    Codigos videos if, else y elif

---
+++ 19082019/001237.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 11:23:27 2019
+
+@author: piedi
+"""
+
+#se muestra como funsiona el if. 
+#Si cambiamos los numeros a e=9 y f=8 se va a imprimir la linea 16, ya que no cumple ni con la 12 ni 14
+e=7
+f=8
+if e<f:
+    print ('e es menor que f')
+elif e==f:
+    print ('e es igual a f')
+else:
+    print ('e es mayor que f')
+    
---
+++ 19082019/001902.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 11:31:30 2019
+
+@author: piedi
+"""
+
+nombre='YK'
+altura_m=2
+peso_kg=90
+
+#ecuacion para calcular sobrepeso. Si es mayor que 25 esta sobre peso, de lo contrario esta bien
+bmi=peso_kg/(altura_m**2)
+print ('bmi:'), (bmi)
+if bmi<25:
+    print (nombre),('no esta sobrepeso')
+else:
+    print (nombre),('esta sobrepeso')
+    
