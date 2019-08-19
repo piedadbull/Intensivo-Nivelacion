74dfd1cfedc924ce6e9fe9a46ce5fff645740259
piedadbull <53713953+piedadbull@users.noreply.github.com>
2019-08-19 17:33:12 -0400

    Add files via upload

---
+++ 22082019/000459.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 17 23:35:55 2019
+
+@author: piedi
+"""
+
+c=list(range(1,5))
+print (c)
+
+for i in range(1,5):
+    print (i)
---
+++ 22082019/000922.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 16:09:25 2019
+
+@author: piedi
+"""
+#se crea un total=0 como variable, la cual despues en el ciclo for va a ir sumando los numeros
+total=0
+
+for i in range(1,100):
+    #multiplo de 3, al total se le suma el numero i
+    if i%3==0:
+        total+=i
+    #multiplo de 5, al total se le suma el numero i.
+    #si usamos if en vez de elif, los numero como el 15, que son multiplos de 3 y de 5 se suman dos veces.
+    elif i%5==0:
+        total+=i
+    #otro caso no se suma
+    else:
+        total=total
+        
+print (total)
