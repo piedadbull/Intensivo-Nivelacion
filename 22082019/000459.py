e8133879527a52edef389ef89212135747dd2ae0
piedadbull <53713953+piedadbull@users.noreply.github.com>
2019-08-19 17:36:13 -0400

    For Loops

--- 22082019/000459.py
+++ 22082019/000459.py
@@ -5,8 +5,10 @@ Created on Sat Aug 17 23:35:55 2019
 @author: piedi
 """
 
+#imprime lista del 1 al 4 (el rango pero sin contar el ultimo numero)
 c=list(range(1,5))
 print (c)
 
+#recorre la lista imprimiendo cada elemento 
 for i in range(1,5):
     print (i)
