f10e57fe80b97ad4ad8aea858681e74ae0238022
piedad bull <pbull@miuandes.cl>
2019-08-19 13:50:03 -0400

    Revert "Update 001237.py"
    This reverts commit 2d95d1dbf83f4150acce12026edbac17b6bd2e3e.

--- 19082019/001237.py
+++ 19082019/001237.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 11:23:27 2019
 
+@author: piedi
+"""
 
 #se muestra como funsiona el if. 
-#Si cambiamos los numeros a e=9 y f=8 se va a imprimir la linea 16, ya que no cumple ni con la linea 12 ni 14
+#Si cambiamos los numeros a e=9 y f=8 se va a imprimir la linea 16, ya que no cumple ni con la 12 ni 14
 e=7
 f=8
 if e<f:
